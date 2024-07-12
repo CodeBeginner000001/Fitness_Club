a44cfc26ecb33bbfb7bb656857c0293b3d117686
Adarsh <ashu2100ag@gmail.com>
2024-07-12 21:45:24 +0530

    Fixed bugs

--- src/Components/SimilarExercises.jsx
+++ src/Components/SimilarExercises.jsx
@@ -6,12 +6,12 @@ export default function SimilarExercises({targetMuscleExercises,equipmentExercis
   return (
     <Box sx={{mt:{lg:'100px',xs:'0'}}}>
       <Typography variant='h3' mb={5} ml={3}>Exercise that <span style={{color:"#ff2625"}}>Target</span> the same muscle group</Typography>
-      <Stack direction="row" sx={{p:'2',position:'relative'}}>
+      <Stack direction="row" sx={{p:'2',position:'relative',overflowY:"hidden"}}>
         {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises}/>:<Loader/>}
       </Stack>
 
       <Typography variant='h3' mb={5} ml={3}>Exercise that uses same <span style={{color:"#ff2625"}}>Equipment</span></Typography>
-      <Stack direction="row" sx={{p:'2',position:'relative'}}>
+      <Stack direction="row" sx={{p:'2',position:'relative',overflowY:"hidden"}}>
         {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises}/>:<Loader/>}
       </Stack>
     </Box>
