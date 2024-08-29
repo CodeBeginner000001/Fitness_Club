89ff10c703711a3c1b8ba6527debc210ddfbcf93
Adarsh <ashu2100ag@gmail.com>
2024-08-29 22:33:59 +0530

    add

--- src/Utils/fetchData.js
+++ src/Utils/fetchData.js
@@ -3,7 +3,7 @@ export const exerciseOptions={
     method: 'GET',
 	headers: {
 		'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-		'x-rapidapi-key': '2cd2c89ba7msh9227dfae65653c3p16fa66jsn6face0470b79'
+		'x-rapidapi-key': '2cd2c89ba7msh9227dfae65653c3p16fa66jsn6face0470b79' // ashu2300ag
 	}
 };
 
@@ -11,7 +11,7 @@ export const youtubeOptions = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com',
-		'x-rapidapi-key': '2cd2c89ba7msh9227dfae65653c3p16fa66jsn6face0470b79'
+		'x-rapidapi-key': '2cd2c89ba7msh9227dfae65653c3p16fa66jsn6face0470b79' //ashu2300ag
 	}
 };
 
