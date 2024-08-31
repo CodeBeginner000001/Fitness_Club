648aaf63a7f2143b0073bdb4a9e659ca79ac6331
Adarsh <ashu2100ag@gmail.com>
2024-08-31 15:36:28 +0530

    add

--- src/Utils/fetchData.js
+++ src/Utils/fetchData.js
@@ -4,7 +4,8 @@ export const exerciseOptions={
 	headers: {
 		'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
 		'x-rapidapi-key': import.meta.env.VITE_API_KEY, // ashu2300ag,
-		// 'x-rapidapi-key': "2cd2c89ba7msh9227dfae65653c3p16fa66jsn6face0470b79" // ashu2300ag,
+		// 'x-rapidapi-key': import.meta.env.VITE_API_KEY_1, // ashu2100ag,
+		// 'x-rapidapi-key': import.meta.env.VITE_API_KEY_2, // adarsh001ag,
 
 	}
 };
@@ -14,13 +15,14 @@ export const youtubeOptions = {
 	headers: {
 		'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com',
 		'x-rapidapi-key':  import.meta.env.VITE_API_KEY, //ashu2300ag
-		// 'x-rapidapi-key':  "2cd2c89ba7msh9227dfae65653c3p16fa66jsn6face0470b79", //ashu2300ag
+		// 'x-rapidapi-key': import.meta.env.VITE_API_KEY_1, // ashu2100ag,
+		// 'x-rapidapi-key': import.meta.env.VITE_API_KEY_2, // adarsh001ag,
 	}
 };
 
 export const fetchData=async (url,options)=>{
     const response=await fetch(url,options);
-	// console.log(import.meta.env);
+	console.log(import.meta.env);
     const data=await response.json();
     return data;
 }
